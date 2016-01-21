5b28fde244914904fd1aba26dff6aeddb7491222
Max <monegask1969@gmail.com>
2016-01-21 17:22:52 +0200

    123

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,18 +3,29 @@ package com.company;
 import java.util.Scanner;
 
 import static com.company.ArrayInverter.invert;
+import static com.company.Merge.merge;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter length of Your array: ");
-        int size = scanner.nextInt();
-        System.out.println("Enter numbers of Your array: ");
-        int[] array = new int[size];
-        for (int i = 0; i < array.length; i++) {
-            array[i] = scanner.nextInt();
+        System.out.println("Enter length of Your 1 array: ");
+        int size1 = scanner.nextInt();
+        System.out.println("Enter numbers of Your 1 array: ");
+        int[] array1 = new int[size1];
+        for (int i = 0; i < array1.length; i++) {
+            array1[i] = scanner.nextInt();
         }
-        invert(array);
+        System.out.println("Enter length of Your 2 array: ");
+        int size2 = scanner.nextInt();
+        System.out.println("Enter numbers of Your 2 array: ");
+        int[] array2 = new int[size2];
+        for (int i = 0; i < array2.length; i++) {
+            array2[i] = scanner.nextInt();
+        }
+        merge(array1,array2);
+        // invert(array1);       // инвертирование массива
+
+
     }
 }
